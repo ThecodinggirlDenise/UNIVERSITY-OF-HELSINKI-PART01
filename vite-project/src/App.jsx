c385f8ef507d6a58ea8af3a5f902245bafdcf9de
Deniz <136066030+ThecodinggirlDenise@users.noreply.github.com>
2025-01-05 23:46:50 +0200

    Final Chnages

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-
+//Defining the App component
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
+  //This block of code defines the anecdotes array,this component should also return JSX
+  const anecdot = [
+    'If it hurts, do it LESS.',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
@@ -11,18 +12,20 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ];
+//USESTATE HOOK==>1-You need to import this hook from your library. 2-You need to define a variable and a function to update the variable. 3-You need to call the useState function and pass the initial value of the variable as an argument.
+//In the example below: expected is the variable and setExpected is the function to update the variable. useState(0) is the initial value of the variable.
+  const [selection, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdot.length).fill(0));
 
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
-
-  const handleNextAnecdote = () => {
-    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+  //This function generates a random index within the range of the anecdotes array length and updates the selected state with this random index. This effectively changes the currently displayed anecdote to a random one from the array.
+  const handleNextAnecdot = () => {
+    const randomIndex = Math.floor(Math.random() * anecdot.length);
     setSelected(randomIndex);
   };
 
   const handleVote = () => {
     const newVotes = [...votes];
-    newVotes[selected] += 1;
+    newVotes[selection] += 1;
     setVotes(newVotes);
   };
 
@@ -31,16 +34,16 @@ const App = () => {
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <button onClick={handleVote}>vote</button>
-      <button onClick={handleNextAnecdote}>next anecdote</button>
+      <h1>Anecdote Of The Day</h1>
+      <p>{anecdot[selection]}</p>
+      <p>has {votes[selection]} Votes</p>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleNextAnecdot}>Next Anecdote</button>
 
       <h1>Anecdote with most votes</h1>
       {highestVoteCount > 0 ? (
         <div>
-          <p>{anecdotes[mostVotedIndex]}</p>
+          <p>{anecdot[mostVotedIndex]}</p>
           <p>has {highestVoteCount} votes</p>
         </div>
       ) : (
